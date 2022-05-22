35616986577ac8f03c1534230dae4d50faa54eb9
Kongkon Biswas <kongkonbiswas3241@gmail.com>
2022-05-23 01:08:55 +0600

    add, remove added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer, useRef } from 'react';
+import React, { useCallback, useReducer, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Lists from './components/Lists';
@@ -11,6 +11,11 @@ interface Todo {
 type ActionType = {type: "ADD"; text:string} | {type: "REMOVE"; id: number}
 
 function App() {
+
+  const [myState, setMyState] = useState<Todo>();
+  
+  // useEffect(() => )
+
   function reducer(state:Todo[], action: ActionType) {
     switch(action.type) {
       case "ADD":
@@ -30,7 +35,13 @@ function App() {
   const newTodoRef = useRef<HTMLInputElement>(null)
 
   const onAddTodo = useCallback(() => {
-
+    if(newTodoRef.current?.value) {
+      dispatch({
+        type: "ADD",
+        text: newTodoRef.current.value
+      })
+      newTodoRef.current.value = "";
+    }
   }, [])
 
   return (
@@ -39,7 +50,8 @@ function App() {
       <input type="text" ref={newTodoRef}></input>
       <button onClick={onAddTodo}>Add</button>
       {todos.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
+        <div key={todo.id}>{todo.text}
+        <button onClick={() => dispatch({ type: "REMOVE", id: todo.id})}>Remove</button></div>
       ))}
     </div>
   );
